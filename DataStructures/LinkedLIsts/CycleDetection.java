bd1ed3f35f2b738ba5eff17426707e3f463a5fad
henry azer <67711333+Henry-Azer@users.noreply.github.com>
2021-03-13 22:24:09 +0200

    Update CycleDetection.java

--- DataStructures/LinkedLIsts/CycleDetection.java
+++ DataStructures/LinkedLIsts/CycleDetection.java
@@ -52,10 +52,12 @@ public class CycleDetection {
     }
 
     static boolean hasCycle(SinglyLinkedListNode head) {
+        if (head == null) return false;
+        
         SinglyLinkedListNode fast = head.next;
         SinglyLinkedListNode slow = head;
 
-        while (fast.next != null && slow != null) {
+        while (fast != null && fast.next != null && slow != null) {
             if (fast == slow) return true;
 
             fast = fast.next.next;
