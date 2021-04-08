d22a40ba311c15a689adb6b2f298dc28b8c7b209
Henry-Azer <henryazer@outlook.com>
2021-04-08 19:55:11 +0200

    Solve CaesarCipher

--- ProblemSolving/Algorithms/Strings/CaesarCipher.java
+++ ProblemSolving/Algorithms/Strings/CaesarCipher.java
@@ -9,10 +9,18 @@ public class CaesarCipher {
 
     static String caesarCipher(String s, int k) {
         StringBuilder result = new StringBuilder();
+        k %= 26;
+
         for (char ch: s.toCharArray()) {
-            if ((ch + k) > 'z') result.append((char) ((ch + k) - 26));
-            else if (ch == '-') result.append(ch);
-            else result.append((char) (ch + k));
+            char letter = (char) (ch + k);
+
+            if (ch > 64 && ch < 91) {
+                if (letter > 90) letter = (char) ((letter % 90) + 64);
+                result.append(letter);
+            } else if (ch > 96 && ch < 123) {
+                if ((ch + k) > 122) letter = (char) (((ch + k) % 122) + 96);
+                result.append(letter);
+            } else result.append(ch);
         }
 
         return result.toString();
@@ -21,7 +29,7 @@ public class CaesarCipher {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
@@ -32,11 +40,12 @@ public class CaesarCipher {
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         String result = caesarCipher(s, k);
+        System.out.println(result);
 
-        bufferedWriter.write(result);
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
+//        bufferedWriter.write(result);
+//        bufferedWriter.newLine();
+//
+//        bufferedWriter.close();
 
         scanner.close();
     }
